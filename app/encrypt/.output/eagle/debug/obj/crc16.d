d38e5bee25fa4b28caca09c963eb83030987165e
banxiasheng <834439771@qq.com>
2019-09-10 22:35:13 +0800

    delete bin files

--- app/encrypt/.output/eagle/debug/obj/crc16.d
+++
@@ -1,6 +0,0 @@
-.output/eagle/debug/obj/crc16.o .output/eagle/debug/obj/crc16.d : crc16.c ../include/user_config.h ../flashapi/flash_api.h \
- ../../include/user_interface.h ../../include/os_type.h \
- ../../include/ets_sys.h ../../include/c_types.h \
- ../../include/eagle_soc.h ../../include/ip_addr.h ../../include/queue.h \
- ../../include/spi_flash.h ../../include/spi_flash.h crc16.h \
- ../../include/c_types.h
--- app/onenet/.output/eagle/debug/obj/edp_openssl.d
+++
@@ -1,1 +0,0 @@
-.output/eagle/debug/obj/edp_openssl.o .output/eagle/debug/obj/edp_openssl.d : edp_openssl.c
